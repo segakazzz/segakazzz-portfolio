3784ccd08ee563180cd5c449e233b5b81f98cbce
Kazue Sasatani <segakazue@gmail.com>
2019-11-13 18:24:01 -0600

    Updated port information for HEROKU deployment

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-
+const port = process.env.PORT || 3000
 app.set('view engine', 'pug')
 
 app.use('/', express.static( __dirname + '/dist'))
 
 app.get('/', (req, res) => res.render('index'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log('Example app listening on port ' + port + '!'))
