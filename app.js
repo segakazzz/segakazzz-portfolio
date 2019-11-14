9ae7359d59fe3e2988ea1fd90367fc7c59bf643f
Kazue Sasatani <segakazue@gmail.com>
2019-11-13 20:11:29 -0600

    Updated app.js

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.use('/', express.static( __dirname + '/dist'))
 
 app.get('/', (req, res) => res.render('index'))
 
+app.get('/.well-known/acme-challenge/VwBy86OeMRrmRwziEZ6UTcYFJQzqQsBecVidF0jC2Hc', function(req, res) {
+    res.send('VwBy86OeMRrmRwziEZ6UTcYFJQzqQsBecVidF0jC2Hc.P-nISRLKrfFIzO2T29KnXqBEDoG82tLLzpPbm7R6htE')
+})
+
 app.listen(port, () => console.log('Example app listening on port ' + port + '!'))
